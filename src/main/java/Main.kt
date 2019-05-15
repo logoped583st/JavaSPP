e2d1dcf9cd5e3dc34ab426d75f8658e2747e1c49
Stas <stanislau@airfox.com>
2019-05-15 21:33:40 +0300

    change state machine

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -2,8 +2,25 @@ class Main{
     companion object{
         @JvmStatic
         fun main(args:Array<String>){
-            Avtomat.BAR.createFactory().createItem().getEffect()
-            Avtomat.JUICE.createFactory().createItem().getEffect()
+            println(StateMacine.getLastState()?.fileText)
+            ChangeFile("change0")
+            println(StateMacine.getLastState()?.fileText)
+            Save()
+            println(StateMacine.getLastState()?.fileText)
+            RoleBack()
+            println(StateMacine.getLastState()?.fileText)
+            ChangeFile("change1")
+            println(StateMacine.getLastState()?.fileText)
+            RoleBack()
+            println(StateMacine.getLastState()?.fileText)
+            ChangeFile("change2")
+            println(StateMacine.getLastState()?.fileText)
+            ChangeFile("change3")
+            println(StateMacine.getLastState()?.fileText)
+            ChangeFile("change4")
+            println(StateMacine.getLastState()?.fileText)
+            RoleBackAll()
+            println(StateMacine.getLastState()?.fileText)
         }
     }
 }
--- src/main/java/Task3.kt
+++ src/main/java/Task3.kt
@@ -1,32 +1,99 @@
-class StateMacine {
+import java.util.*
 
-    private val history: List<State> = ArrayList()
+object StateMacine {
+
+    private val history: Stack<State> = Stack()
+
+    fun getLastState(): State? = history.lastOrNull()
+
+    init {
+        InitState()
+    }
 
     fun enterEvent(state: State, actions: Actions) {
         when (actions) {
-            is Actions.RollBack -> {
-
+            is Actions.RollBackLast -> {
+                if (history.size > 1) {
+                    history.pop()
+                }
             }
             is Actions.Save -> {
-
+                history.clear()
+                history.push(state)
             }
-            is Actions.PushEvent -> {
-
+            is Actions.Change -> {
+                history.push(state)
+            }
+            is Actions.Init -> {
+                history.push(state)
+            }
+            is Actions.RollBackAll -> {
+                val story = history.firstElement()
+                history.clear()
+                history.push(story)
             }
         }
+        println(actions)
+    }
+
+}
+
+class Save : State {
+
+    override var fileText: String = StateMacine.getLastState()?.fileText ?: ""
+
+    init {
+        StateMacine.enterEvent(this, Actions.Save)
+    }
+
+
+}
+
+class ChangeFile(val changeFor: String) : State {
+    override var fileText: String = StateMacine.getLastState()?.fileText ?: ""
+
+    init {
+        fileText = changeFor
+        StateMacine.enterEvent(this, Actions.Change)
+    }
+
+}
+
+class InitState : State {
+
+    override var fileText: String = "init file"
+
+    init {
+        StateMacine.enterEvent(this, Actions.Init)
     }
 }
 
+class RoleBackAll : State {
+    override var fileText: String = StateMacine.getLastState()?.fileText ?: ""
 
+    init {
+        StateMacine.enterEvent(this, Actions.RollBackAll)
+    }
+}
 
-abstract class State() {
-    abstract val state: State
+class RoleBack : State {
+    override var fileText: String = StateMacine.getLastState()?.fileText ?: ""
+
+    init {
+        StateMacine.enterEvent(this, Actions.RollBackLast)
+    }
+}
+
+interface State {
+    var fileText: String
 }
 
 sealed class Actions {
-    object RollBack : Actions()
+    object RollBackAll : Actions()
+    object RollBackLast : Actions()
     object Save : Actions()
-    object PushEvent : Actions()
+    object Change : Actions()
+    object Init : Actions()
 }
 
 
